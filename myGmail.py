c8ac3b9b44d5e8e768c606a4c51c4358cc16a37d
Fan Yang <wyns.smart@gmail.com>
2018-07-13 21:08:48 -0700

    Update myGmail.py

--- myGmail.py
+++ myGmail.py
@@ -1,29 +1,36 @@
 import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 
-def send_email(recipient, subject, body):
+def send_email(recipient, subject, html_body):
 
     gmail_user = 'wynsmart.test'
     gmail_pwd = 'wynsmart'
-    FROM = '{}@gmail.com'.format(gmail_user)
-    TO = recipient if type(recipient) is list else [recipient]
-    SUBJECT = subject
-    TEXT = body
+    me = '{}@gmail.com'.format(gmail_user)
+    you = recipient
 
-    # Prepare actual message
-    message = "\From: {}\nTo: {}\nSubject: {}\n\n{}".format(
-        FROM, ", ".join(TO), SUBJECT, TEXT)
+    # Create message container - the correct MIME type is multipart/alternative.
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = subject
+    msg['From'] = me
+    msg['To'] = you
+    msg.attach(MIMEText(html_body, 'html'))
+
     try:
         server = smtplib.SMTP("smtp.gmail.com", 587)
         server.ehlo()
         server.starttls()
         server.login(gmail_user, gmail_pwd)
-        server.sendmail(FROM, TO, message)
+        server.sendmail(me, you, msg.as_string())
         server.close()
-        print('EMAIL SENT')
+        return True
     except Exception as e:
-        print(e)
+        if __name__ == '__main__':
+            print(e)
+        return False
 
 
 if __name__ == '__main__':
-    send_email('wyns.smart@gmail.com', 'test message', 'Job is done.')
+    send_email('wyns.smart@gmail.com', 'test message', '<strong>Job is done.</strong>')
+    print('EMAIL SENT')
